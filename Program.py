3cc2ba1cba6439b0aa3e3e79fef0aa78f6b1d32a
Wander1ngSoul <ignatsidorov06@mail.ru>
2025-02-26 22:05:20 +0300

    Исправление ошибок и оптимизация кода

--- Program.py
+++ Program.py
@@ -4,67 +4,47 @@ import easyocr
 import re
 import matplotlib.pyplot as plt
 
-
 reader = easyocr.Reader(['ru'])
 
 def convert_to_grayscale(image):
-
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 def process_image(image_path):
-
-    # Загрузка изображения
     image = cv2.imread(image_path)
-
-    # Преобразование в оттенки серого
     gray_image = convert_to_grayscale(image)
-
     _, binary_image = cv2.threshold(gray_image, 128, 255, cv2.THRESH_BINARY)
-
     result = reader.readtext(gray_image)
-
     print(f"\nОбработка изображения: {image_path}")
     combined_numbers = ""
-
     for detection in result:
         text = detection[1]
         text = text.replace('@', '0')
         numbers = re.sub(r'[^0-9.,]', '', text)
         if numbers:
             combined_numbers += numbers
-
-
     print(f"Числа: {combined_numbers}")
-
-    # Визуализация изображений
     plt.figure(figsize=(15, 10))
-
-
     plt.subplot(2, 2, 1)
     plt.title("Оригинал")
     plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
     plt.axis('off')
-
     plt.subplot(2, 2, 2)
     plt.title("Оттенки серого")
     plt.imshow(gray_image, cmap='gray')
     plt.axis('off')
-
     plt.subplot(2, 2, 3)
     plt.title("Бинаризация")
     plt.imshow(binary_image, cmap='gray')
     plt.axis('off')
-
     plt.subplot(2, 2, 4)
     plt.title("Распознанный текст")
-    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))  # Отображаем оригинальное изображение
+    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
     for detection in result:
         top_left = tuple(map(int, detection[0][0]))
         bottom_right = tuple(map(int, detection[0][2]))
         cv2.rectangle(image, top_left, bottom_right, (0, 255, 0), 2)
     plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
     plt.axis('off')
-
     plt.show()
 
 folder_path = 'all_data/ru_val'
